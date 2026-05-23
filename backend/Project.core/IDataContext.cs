da7c9d4cf8f4fb64dbbfa0932126ebfe75c41970
R32917 <X0583291774@GMAIL.COM>
2026-05-23 22:48:04 +0300

    Remove backend - moved to separate repository

--- backend/Project.core/IDataContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace project
-{
-    public interface IDataContext
-    {
-        public List<Babies> babies { get; set; }
-        public List<Nurses> nurses { get; set; }
-        public List<Turns> turns { get; set; }
-    }
-}
